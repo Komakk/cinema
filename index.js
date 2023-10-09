ca2a0960cab6731f56f7e7feadd1d7f8942769f0
Anton Mikhailov <komakkomakk@gmail.com>
2023-10-09 06:20:37 +0400

    import data

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+import { data } from "./data";
 
 let days = document.getElementsByClassName('day');
 let today = Date.now();
 for (const day of days) {
     let date = new Date(today);
-    day.textContent = date.getDate() + '.' + (date.getMonth() + 1);
+    let dayOfMounth = date.getDate() + '.' + (date.getMonth() + 1);
+    day.textContent = dayOfMounth;
     today += 86400000;
 }
