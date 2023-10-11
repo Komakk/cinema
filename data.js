4dafe6c0aacafc52e1f64d6714f93000f1cec71c
Anton Mikhailov <komakkomakk@gmail.com>
2023-10-11 17:29:43 +0400

    change data.js

--- data.js
+++ data.js
@@ -1,58 +1,58 @@
 export const data = {
     '9.10': [
-        {'name': 'Valkiria', 'seats': [0,0,0,0,1,1,1,0,0,0]},
-        {'name': 'Love', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shame', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Mission Imposible', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shrek', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Iron Man', 'seats': [0,1,1,1,1,1,1,1,1,0]}
+        {'name': 'Valkiria', 'session': '10:00', 'seats': [0,0,0,0,1,1,1,0,0,0]},
+        {'name': 'Love', 'session': '12:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shame', 'session': '14:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Mission Imposible', 'session': '16:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shrek', 'session': '18:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Iron Man', 'session': '20:00', 'seats': [0,1,1,1,1,1,1,1,1,0]}
     ],
     '10.10': [
-        {'name': 'Valkiria', 'seats': [0,0,0,0,1,1,1,0,0,0]},
-        {'name': 'Love', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shame', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Mission Imposible', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shrek', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Iron Man', 'seats': [0,1,1,1,1,1,1,1,1,0]}
+        {'name': 'Valkiria', 'session': '10:00', 'seats': [0,0,0,0,1,1,1,0,0,0]},
+        {'name': 'Love', 'session': '12:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shame', 'session': '14:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Mission Imposible', 'session': '16:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shrek', 'session': '18:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Iron Man', 'session': '20:00', 'seats': [0,1,1,1,1,1,1,1,1,0]}
     ],
     '11.10': [
-        {'name': 'Valkiria', 'seats': [0,0,0,0,1,1,1,0,0,0]},
-        {'name': 'Love', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shame', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Mission Imposible', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shrek', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Iron Man', 'seats': [0,1,1,1,1,1,1,1,1,0]}
+        {'name': 'Valkiria', 'session': '10:00', 'seats': [0,0,0,0,1,1,1,0,0,0]},
+        {'name': 'Love', 'session': '12:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shame', 'session': '14:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Mission Imposible', 'session': '16:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shrek', 'session': '18:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Iron Man', 'session': '20:00', 'seats': [0,1,1,1,1,1,1,1,1,0]}
     ],
     '12.10': [
-        {'name': 'Valkiria', 'seats': [0,0,0,0,1,1,1,0,0,0]},
-        {'name': 'Love', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shame', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Mission Imposible', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shrek', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Iron Man', 'seats': [0,1,1,1,1,1,1,1,1,0]}
+        {'name': 'Valkiria', 'session': '10:00', 'seats': [0,0,0,0,1,1,1,0,0,0]},
+        {'name': 'Love', 'session': '12:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shame', 'session': '14:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Mission Imposible', 'session': '16:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shrek', 'session': '18:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Iron Man', 'session': '20:00', 'seats': [0,1,1,1,1,1,1,1,1,0]}
     ],
     '13.10': [
-        {'name': 'Valkiria', 'seats': [0,0,0,0,1,1,1,0,0,0]},
-        {'name': 'Love', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shame', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Mission Imposible', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shrek', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Iron Man', 'seats': [0,1,1,1,1,1,1,1,1,0]}
+        {'name': 'Valkiria', 'session': '10:00', 'seats': [0,0,0,0,1,1,1,0,0,0]},
+        {'name': 'Love', 'session': '12:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shame', 'session': '14:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Mission Imposible', 'session': '16:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shrek', 'session': '18:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Iron Man', 'session': '20:00', 'seats': [0,1,1,1,1,1,1,1,1,0]}
     ],
     '14.10': [
-        {'name': 'Valkiria', 'seats': [0,0,0,0,1,1,1,0,0,0]},
-        {'name': 'Love', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shame', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Mission Imposible', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shrek', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Iron Man', 'seats': [0,1,1,1,1,1,1,1,1,0]}
+        {'name': 'Valkiria', 'session': '10:00', 'seats': [0,0,0,0,1,1,1,0,0,0]},
+        {'name': 'Love', 'session': '12:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shame', 'session': '14:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Mission Imposible', 'session': '16:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shrek', 'session': '18:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Iron Man', 'session': '20:00', 'seats': [0,1,1,1,1,1,1,1,1,0]}
     ],
     '15.10': [
-        {'name': 'Valkiria', 'seats': [0,0,0,0,1,1,1,0,0,0]},
-        {'name': 'Love', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shame', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Mission Imposible', 'seats': [0,1,0,1,1,1,1,0,0,0]},
-        {'name': 'Shrek', 'seats': [0,1,0,1,1,1,1,1,0,0]},
-        {'name': 'Iron Man', 'seats': [0,1,1,1,1,1,1,1,1,0]}
+        {'name': 'Valkiria', 'session': '10:00', 'seats': [0,0,0,0,1,1,1,0,0,0]},
+        {'name': 'Love', 'session': '12:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shame', 'session': '14:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Mission Imposible', 'session': '16:00', 'seats': [0,1,0,1,1,1,1,0,0,0]},
+        {'name': 'Shrek', 'session': '18:00', 'seats': [0,1,0,1,1,1,1,1,0,0]},
+        {'name': 'Iron Man', 'session': '20:00', 'seats': [0,1,1,1,1,1,1,1,1,0]}
     ],
 }
