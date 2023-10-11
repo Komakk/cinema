c9861cb0cb00afa287cf1228e4d9d2829b1b10f1
Anton Mikhailov <komakkomakk@gmail.com>
2023-10-11 06:23:41 +0400

    add listeners to seat

--- index.js
+++ index.js
@@ -8,21 +8,28 @@ for (const day of days) {
     day.textContent = dayOfMounth;
     today += 86400000;
 
-    day.addEventListener('click', updateSessions(dayOfMounth));
+    day.addEventListener('click', () => {
+        updateSessions(dayOfMounth);
+    });
 }
-//updateSessions('10.10');
+updateSessions('10.10');
 
 function updateSessions(day) {
     let movies = data[day];
     let sessions = document.getElementsByClassName('session');
     Array.from(sessions).forEach((el, i) => {
         let movie = movies[i];
-        let movieName = document.createTextNode(movie.name);
-        el.appendChild(movieName);
-        el.addEventListener('click',updateCurrentSession(movie));
+        let movieName = el.getElementsByTagName('p')[0];
+        movieName.textContent = movie.name;
+        el.addEventListener('click', () => {
+            updateCurrentSession(movie);
+        });
     });
 }
 
 function updateCurrentSession(movie) {
-
+    let seats = document.getElementsByClassName('seat');
+    Array.from(seats).forEach((el, i) => {
+        el.style.backgroundColor = movie.seats[i] ? 'aquamarine' : 'black'; 
+    })
 }
